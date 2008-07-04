10345f4945a73ba78af7c0d7e54c32368e172dfa
lolli <lolli@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-07-04 17:34:43 +0000

    Feature 977: Respect rights in overview
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/passwordmgr/trunk@10388 735d13b6-9817-0410-8766-e36946ffe9aa

--- view/class.tx_passwordmgr_view_overview.php
+++ view/class.tx_passwordmgr_view_overview.php
@@ -46,6 +46,19 @@ class tx_passwordmgr_view_overview extends tx_passwordmgr_view_default {
 
 		$groupContent = array();
 		foreach ( $groupList as $group ) {
+			// Determine user rights and number of group admin users of this group
+			$self = t3lib_div::makeInstance('tx_passwordmgr_model_groupMember');
+			$self->init($GLOBALS['BE_USER']->user['uid'], $group['uid']);
+			$rights = $self['rights'];
+			$memberList = $group->getMemberList();
+			$numberOfGroupAdmins = 0;
+			foreach ( $memberList as $member ) {
+				if ( $member['rights'] == 2 ) {
+					$numberOfGroupAdmins++;
+				}
+			}
+
+			// Only show group content if group is expanded
 			$isOpenGroup = $userData->isOpen('group',$group['uid']);
 
 			$memberContent = array();
@@ -54,7 +67,10 @@ class tx_passwordmgr_view_overview extends tx_passwordmgr_view_default {
 			if ( $isOpenGroup ) {
 				// Member list
 				$isOpenMember = $userData->isOpen('member',$group['uid']);
-				$memberList = $group->getMemberList();
+				$newMemberIcon = '';
+				if ( $rights == 2 ) {
+					$newMemberIcon = '<img style="cursor: pointer;" '.t3lib_iconWorks::skinImg($backPath, 'gfx/new_el.gif').' onclick="setFieldValue(\'groupUid\', \''.$group['uid'].'\'); setFieldValue(\'view\', \'addEditGroupMember\'); document.passwordmgr.submit();" alt="Add new member" title="Add new member" />';
+				}
 				$memberContent[] = '
 					<tr>
 						<td></td>
@@ -63,13 +79,16 @@ class tx_passwordmgr_view_overview extends tx_passwordmgr_view_default {
 						</td>
 						<td class="bgColor5" colspan="2">Member: '.count($memberList).'</td>
 						<td class="bgColor5">
-							<img style="cursor: pointer;" '.t3lib_iconWorks::skinImg($backPath, 'gfx/new_el.gif').' onclick="setFieldValue(\'groupUid\', \''.$group['uid'].'\'); setFieldValue(\'view\', \'addEditGroupMember\'); document.passwordmgr.submit();" alt="Add new member" title="Add new member" />
+							'.$newMemberIcon.'
 						</td>
 					</tr>
 				';
+				
+				// Show member list if list is not folded
 				if ( $isOpenMember ) {
 					$bgColor = 'bgColor4';
 					foreach ( $memberList as $member ) {
+						// Show current member rights
 						if ( $member['rights'] == 0 ) {
 							$rightsContent = 'view';
 						} elseif ( $member['rights'] == 1 ) {
@@ -77,14 +96,26 @@ class tx_passwordmgr_view_overview extends tx_passwordmgr_view_default {
 						} else {
 							$rightsContent = 'admin';
 						}
+
+						// Determine if delete and edit icons should be shown
+						$editMemberIcon = '';
+						$deleteMemberIcon = '';
+						if ( $rights == 2 ) {
+							// Show icons for admin members only if there is a second admin
+							if ( ($member['rights'] == 2 && $numberOfGroupAdmins >= 2) || $member['rights'] < 2 ) {
+								$editMemberIcon = '<img style="cursor: pointer;" '.t3lib_iconWorks::skinImg($backPath, 'gfx/edit2.gif').' onclick="setFieldValue(\'view\', \'addEditGroupMember\'); setFieldValue(\'groupUid\', \''.$group['uid'].'\'); setFieldValue(\'groupMemberUid\', \''.$member['beUserUid'].'\'); document.passwordmgr.submit();" alt="Edit member" title="Edit member" />';
+								$deleteMemberIcon = '<img style="cursor: pointer;" '.t3lib_iconWorks::skinImg($backPath, 'gfx/garbage.gif').' onclick="if (confirm(\'Are you sure you want to delete this member?\')) {setAction(\'deleteGroupMember\'); setFieldValue(\'groupUid\', \''.$group['uid'].'\'); setFieldValue(\'groupMemberUid\', \''.$member['beUserUid'].'\'); document.passwordmgr.submit();}" alt="Delete member" title="Delete member" />';
+							}
+						}
+
 						$memberContent[] = '
 							<tr>
 								<td colspan="2"></td>
 								<td class="'.$bgColor.'">'.$member['name'].'</td>
 								<td class="'.$bgColor.'">'.$rightsContent.'</td>
 								<td class="'.$bgColor.'">
-									<img style="cursor: pointer;" '.t3lib_iconWorks::skinImg($backPath, 'gfx/edit2.gif').' onclick="setFieldValue(\'view\', \'addEditGroupMember\'); setFieldValue(\'groupUid\', \''.$group['uid'].'\'); setFieldValue(\'groupMemberUid\', \''.$member['beUserUid'].'\'); document.passwordmgr.submit();" alt="Edit member" title="Edit member" />
-									<img style="cursor: pointer;" '.t3lib_iconWorks::skinImg($backPath, 'gfx/garbage.gif').' onclick="if (confirm(\'Are you sure you want to delete this member?\')) {setAction(\'deleteGroupMember\'); setFieldValue(\'groupUid\', \''.$group['uid'].'\'); setFieldValue(\'groupMemberUid\', \''.$member['beUserUid'].'\'); document.passwordmgr.submit();}" alt="Delete member" title="Delete member" />
+									'.$editMemberIcon.'
+									'.$deleteMemberIcon.'
 								</td>
 							</tr>
 						';
@@ -96,13 +127,16 @@ class tx_passwordmgr_view_overview extends tx_passwordmgr_view_default {
 				$isOpenPassword = $userData->isOpen('password',$group['uid']);
 				$passwordList = $group->getPasswordList();
 
-				// Paste (move) password button
-				if ( strlen($userData['selectedPassword'])>0 ) {
-					$pastePasswordContent = '<img style="cursor: pointer;" '.t3lib_iconWorks::skinImg($backPath, 'gfx/clip_pasteafter.gif').' onclick="setAction(\'movePassword\'); setFieldValue(\'groupUid\', \''.$group['uid'].'\'); passphrasePopUp();" alt="Paste password" title="Paste password" />';
-				} else {
-					$pastePasswordContent = '';
+				// Paste (move) password icon
+				$pastePasswordIcon = '';
+				if ( strlen($userData['selectedPassword'])>0 && $rights>=1 ) {
+					$pastePasswordIcon = '<img style="cursor: pointer;" '.t3lib_iconWorks::skinImg($backPath, 'gfx/clip_pasteafter.gif').' onclick="setAction(\'movePassword\'); setFieldValue(\'groupUid\', \''.$group['uid'].'\'); passphrasePopUp();" alt="Paste password" title="Paste password" />';
 				}
-
+				// New password icon
+				$newPasswordIcon = '';
+				if ( $rights >= 1 ) {
+					$newPasswordIcon = '<img style="cursor: pointer;" '.t3lib_iconWorks::skinImg($backPath, 'gfx/new_el.gif').' onclick="setFieldValue(\'groupUid\', \''.$group['uid'].'\'); setFieldValue(\'view\', \'addEditPassword\'); document.passwordmgr.submit();" alt="Add new password" title="Add new password" />';
+				}
 				$passwordContent[] = '
 					<tr>
 						<td></td>
@@ -111,25 +145,43 @@ class tx_passwordmgr_view_overview extends tx_passwordmgr_view_default {
 						</td>
 						<td class="bgColor5" colspan="2">Passwords: '.count($passwordList).'</td>
 						<td class="bgColor5">
-							<img style="cursor: pointer;" '.t3lib_iconWorks::skinImg($backPath, 'gfx/new_el.gif').' onclick="setFieldValue(\'groupUid\', \''.$group['uid'].'\'); setFieldValue(\'view\', \'addEditPassword\'); document.passwordmgr.submit();" alt="Add new password" title="Add new password" />
-							'.$pastePasswordContent.'
+							'.$newPasswordIcon.'
+							'.$pastePasswordIcon.'
 						</td>
 					</tr>
 				';
+
+				// Show password list if list is not folded
 				if ( $isOpenPassword ) {
 					$bgColor = 'bgColor4';
 					foreach ( $passwordList as $password ) {
+						// Wrap password name with link if link is set
 						if ( strlen($password['link'])>0 ) {
 							$nameAndLinkContent = '<a target="_blank" href="'.$password['link'].'">'.$password['name'].'</a>';
 						} else {
 							$nameAndLinkContent = $password['name'];
 						}
+
+						// Show plaintext password below username if set in data
 						$userAndPasswordContent = 'User: '.$password['user'];
 						if ( isset($this->data['plaintextPassword'][$password['uid']]) ) {
 							$userAndPasswordContent .= '<br />Password: '.$this->data['plaintextPassword'][$password['uid']];
 						}
-						// Determine if the password is selected in user data uc
-						$isSel = $userData->isSelectedPassword($password['uid']);
+
+						$decryptPasswordIcon = '<img style="cursor: pointer;" src="../res/decrypted.png" onclick="setAction(\'decryptPassword\'); setFieldValue(\'passwordUid\', \''.$password['uid'].'\'); passphrasePopUp();" alt="Decrypt password" title="Decrypt password" />';
+
+						// Show icons if user has edit rights
+						$editPasswordIcon = '';
+						$cutPasswordIcon = '';
+						$deletePasswordIcon = '';
+						if ( $rights >= 1 ) {
+							$editPasswordIcon = '<img style="cursor: pointer;" '.t3lib_iconWorks::skinImg($backPath, 'gfx/edit2.gif').' onclick="setFieldValue(\'view\', \'addEditPassword\'); setFieldValue(\'groupUid\', \''.$group['uid'].'\'); setFieldValue(\'passwordUid\', \''.$password['uid'].'\'); document.passwordmgr.submit();" alt="Edit password" title="Edit password" />';
+							// Determine if the password is selected in user data uc
+							$isSel = $userData->isSelectedPassword($password['uid']);
+							$cutPasswordIcon = '<img style="cursor: pointer;" '.t3lib_iconWorks::skinImg($backPath, 'gfx/clip_cut'.($isSel?'_h':'').'.gif').' onclick="setAction(\''.($isSel?'de':'').'selectPassword\'); setFieldValue(\'passwordUid\', \''.$password['uid'].'\'); document.passwordmgr.submit();" alt="'.($isSel?'':'Cut password').'" title="'.($isSel?'':'Cut password').'" />';
+
+							$deletePasswordIcon = '<img style="cursor: pointer;" '.t3lib_iconWorks::skinImg($backPath, 'gfx/garbage.gif').' onclick="if (confirm(\'This action cannot be undone! Are you sure you want to delete this password?\')) {setAction(\'deletePassword\'); setFieldValue(\'passwordUid\', \''.$password['uid'].'\'); document.passwordmgr.submit();}" alt="Delete password" title="Delete password" />';
+						}
 
 						$passwordContent[] = '
 							<tr>
@@ -137,10 +189,10 @@ class tx_passwordmgr_view_overview extends tx_passwordmgr_view_default {
 								<td class="'.$bgColor.'">'.$nameAndLinkContent.'</td>
 								<td class="'.$bgColor.'">'.$userAndPasswordContent.'</td>
 								<td class="'.$bgColor.'">
-									<img style="cursor: pointer;" '.t3lib_iconWorks::skinImg($backPath, 'gfx/edit2.gif').' onclick="setFieldValue(\'view\', \'addEditPassword\'); setFieldValue(\'groupUid\', \''.$group['uid'].'\'); setFieldValue(\'passwordUid\', \''.$password['uid'].'\'); document.passwordmgr.submit();" alt="Edit password" title="Edit password" />
-									<img style="cursor: pointer;" '.t3lib_iconWorks::skinImg($backPath, 'gfx/clip_cut'.($isSel?'_h':'').'.gif').' onclick="setAction(\''.($isSel?'de':'').'selectPassword\'); setFieldValue(\'passwordUid\', \''.$password['uid'].'\'); document.passwordmgr.submit();" alt="'.($isSel?'':'Cut password').'" title="'.($isSel?'':'Cut password').'" />
-									<img style="cursor: pointer;" '.t3lib_iconWorks::skinImg($backPath, 'gfx/garbage.gif').' onclick="if (confirm(\'This action cannot be undone! Are you sure you want to delete this password?\')) {setAction(\'deletePassword\'); setFieldValue(\'passwordUid\', \''.$password['uid'].'\'); document.passwordmgr.submit();}" alt="Delete password" title="Delete password" />
-									<img style="cursor: pointer;" src="../res/decrypted.png" onclick="setAction(\'decryptPassword\'); setFieldValue(\'passwordUid\', \''.$password['uid'].'\'); passphrasePopUp();" alt="Decrypt password" title="Decrypt password" />
+									'.$editPasswordIcon.'
+									'.$cutPasswordIcon.'
+									'.$deletePasswordIcon.'
+									'.$decryptPasswordIcon.'
 								</td>
 							</tr>
 						';
@@ -149,6 +201,13 @@ class tx_passwordmgr_view_overview extends tx_passwordmgr_view_default {
 				} // End of if password list is open
 			} // End of if group is open
 
+			// Show edit and delete group icons if user is group admin
+			$editGroupIcon = '';
+			$deleteGroupIcon = '';
+			if ( $rights == 2 ) {
+				$editGroupIcon = '<img style="cursor: pointer;" '.t3lib_iconWorks::skinImg($backPath, 'gfx/edit2.gif').' onclick="setFieldValue(\'view\', \'addEditGroup\'); setFieldValue(\'groupUid\', \''.$group['uid'].'\'); document.passwordmgr.submit();" alt="Edit group" title="Edit group" />';
+				$deleteGroupIcon = '<img style="cursor: pointer;" '.t3lib_iconWorks::skinImg($backPath, 'gfx/garbage.gif').' onclick="if (confirm(\'This will also delete all passwords of this group! This action cannot be undone! Are you sure you want to delete this group?\')) {setAction(\'deleteGroup\'); setFieldValue(\'groupUid\', \''.$group['uid'].'\'); document.passwordmgr.submit();}" alt="Delete group" title="Delete group" />';
+			}
 			// Content of this group
 			$groupContent[] = '
 				<tr>
@@ -157,8 +216,8 @@ class tx_passwordmgr_view_overview extends tx_passwordmgr_view_default {
 					</td>
 					<td class="bgColor2" colspan="3">Group: '.$group['name'].'</td>
 					<td class="bgColor2">
-						<img style="cursor: pointer;" '.t3lib_iconWorks::skinImg($backPath, 'gfx/edit2.gif').' onclick="setFieldValue(\'view\', \'addEditGroup\'); setFieldValue(\'groupUid\', \''.$group['uid'].'\'); document.passwordmgr.submit();" alt="Edit group" title="Edit group" />
-						<img style="cursor: pointer;" '.t3lib_iconWorks::skinImg($backPath, 'gfx/garbage.gif').' onclick="if (confirm(\'This will also delete all passwords of this group! This action cannot be undone! Are you sure you want to delete this group?\')) {setAction(\'deleteGroup\'); setFieldValue(\'groupUid\', \''.$group['uid'].'\'); document.passwordmgr.submit();}" alt="Delete group" title="Delete group" />
+						'.$editGroupIcon.'
+						'.$deleteGroupIcon.'
 						'.$this->addNewGroupIcon().'
 					</td>
 				</tr>
